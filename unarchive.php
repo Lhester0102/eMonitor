d280dd3ab173b96b588206b2eb9ca1262868fdcc
albrinjeffzacariaspaleracio07@gmail.com <albrinjeffzacariaspaleracio07@gmail.com>
2024-02-20 14:15:13 +0800

    navigation bar update

--- unarchive.php
+++ unarchive.php
@@ -9,7 +9,57 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">	</head>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 	<body>
-		<h1 align="center">Archive List (Admin)</h1>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+		<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">DCCP</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+			<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="collapsibleNavbar">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+				<a class="nav-link" href="admin-dashboard.php">Dashboard</a>
+				</li>
+				<li class="nav-item">
+				<a class="nav-link" href="#">Supply Officers</a>
+				</li>
+				<li class="nav-item">
+				<a class="nav-link" href="instructor.php">Instructors</a>
+				</li>  
+				<li class="nav-item">
+				<a class="nav-link" href="index.php">Inventory</a>
+				</li>
+				<li class="nav-item">
+				<a class="nav-link" href="#">Reports</a>
+				</li>
+				<!-- <li class="nav-item dropdown" stylesheet="float:right">
+				<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Dropdown</a>
+				<ul class="dropdown-menu">
+					<li><a class="dropdown-item" href="#">Link</a></li>
+					<li><a class="dropdown-item" href="#">Another link</a></li>
+					<li><a class="dropdown-item" href="#">A third link</a></li>
+				</ul>
+				</li> -->
+			</ul>
+			<div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
+			<ul class="navbar-nav">
+			<li class="nav-item">
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">admin@gmail.com</a>
+				<ul class="dropdown-menu">
+					<li><a class="dropdown-item" href="#">Profile</a></li>
+					<li><a class="dropdown-item" href="#">Settings</a></li>
+					<li><a class="dropdown-item" href="log-in.php">Sign Out</a></li>
+				</ul>
+				</li>
+			</li>
+		</ul>
+		</div>
+			</div>
+		</div>
+		</nav>
 		<table class="table table-striped" border="1px" align="center">
 		<thead>
 			<tr align="center">
